2ebe0f5efd8f9a452721c6b8d4afa1cff733bfff
Maksym <smolinskimaksym@gmail.com>
2023-06-14 12:17:38 +0200

    added working ver of fe

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 
-function Header({header}) {
+function Header({header, setInputValue}) {
     return (
         <header className="bg-dark py-5">
         <div className="container px-4 px-lg-5 my-5">
@@ -8,6 +11,16 @@ function Header({header}) {
                 <p className="lead fw-normal text-white-50 mb-0"></p>
             </div>
         </div>
+        <Form className='container'>
+                <Form.Group as={Row} className='m-4'>
+                    <Form.Label column sm="2" style={{color: 'white'}}>
+                        Sos
+                    </Form.Label>
+                    <Col sm="10">
+                        <Form.Control type="number" placeholder="piniadz" onChange={(e) => setInputValue(e.target.value)} />
+                    </Col>
+                </Form.Group>
+            </Form>
     </header>
     )
 }
