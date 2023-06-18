f636830f8d4750ec745c72c91c845d4f651ee541
Voyda2137 <91951966+Voyda2137@users.noreply.github.com>
2023-06-18 22:37:15 +0200

    wyświetlanie warunkowe

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,56 +1,79 @@
 import React from 'react';
-import {alcoholAmountCalc, alcoholTotalPure, alcoholInOrganism} from '../service/AlcoholFunctions'
+import {alcoholAmountCalc, alcoholTotalPure, alcoholInOrganism, timeToBeSober} from '../service/AlcoholFunctions';
+
 function Card({name, price, concentration, size, img, index, inputValue, weightValue}) {
 
-    return (
+    const alcoholAmount = alcoholAmountCalc(price, inputValue)
+    const totalPureAlcohol = alcoholTotalPure(concentration, size, alcoholAmount)
+    const totalAlcoholInOrganism = alcoholInOrganism(totalPureAlcohol, weightValue)
+    const timeToSober = timeToBeSober(totalAlcoholInOrganism)
 
-        <div className="col mb-5" key={index}>
-        <div className={"card h-100"}>
-        {img ? <img className="card-img-top" src={img} alt={[index, name].join('-')} /> : <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt={`${[index,name].join('-')} template 450x300`} />}
-        <div className="card-body p-4">
-        <div className="text-center">
-            <h5 className="fw-bolder">{name}</h5>
-            <div className="d-flex flex-wrap flex-column ">
-            <div className="d-flex justify-content-between">
-                <strong>Price:</strong> <span>{price} zł</span>
-            </div>
-            {concentration && (
-                <div className="d-flex justify-content-between">
-                <strong>Concentration:</strong> <span>{(concentration*100).toFixed(1)}%</span>
-                </div>
-            )}
-            {size && (
-                <div className="d-flex justify-content-between">
-                <strong>Size:</strong> <span>{size} ml</span>
-                </div>
-            )}
-            {inputValue && (
-                <>
-                <div className='d-flex justify-content-between'>
-                    <strong>Możesz kupić:</strong>
-                    <span>{alcoholAmountCalc(price, inputValue)}</span>
-                </div>
-                <div className='d-flex justify-content-between'>
-                <strong>Łącznie alkoholu:</strong>
-                <span>{alcoholTotalPure(concentration, size, alcoholAmountCalc(price, inputValue))} ml</span>
-                </div>
-                    <div className='d-flex justify-content-between'>
-                        <strong>Promile po wypiciu:</strong>
-                        <span>{alcoholInOrganism(alcoholTotalPure(concentration, size, alcoholAmountCalc(price, inputValue)), weightValue)}‰</span>
+    return (
+        <>
+            {alcoholAmount !== 0 &&
+                (<div className="col mb-5" key={index}>
+                    <div className={"card h-100"}>
+                        {img ? (
+                            <img className="card-img-top" src={img} alt={[index, name].join('-')}/>
+                        ) : (
+                            <img
+                                className="card-img-top"
+                                src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+                                alt={`${[index, name].join('-')} template 450x300`}
+                            />
+                        )}
+                        <div className="card-body p-4">
+                            <div className="text-center">
+                                <h5 className="fw-bolder">{name}</h5>
+                                <div className="d-flex flex-wrap flex-column ">
+                                    <div className="d-flex justify-content-between">
+                                        <strong>Price:</strong> <span>{price} zł</span>
+                                    </div>
+                                    {concentration && (
+                                        <div className="d-flex justify-content-between">
+                                            <strong>Concentration:</strong>
+                                            <span>{(concentration * 100).toFixed(1)}%</span>
+                                        </div>
+                                    )}
+                                    {size && (
+                                        <div className="d-flex justify-content-between">
+                                            <strong>Size:</strong> <span>{size} ml</span>
+                                        </div>
+                                    )}
+                                    {inputValue && (
+                                        <>
+                                            <div className='d-flex justify-content-between'>
+                                                <strong>Możesz kupić:</strong>
+                                                <span>{alcoholAmount}</span>
+                                            </div>
+                                            <div className='d-flex justify-content-between'>
+                                                <strong>Łącznie alkoholu:</strong>
+                                                <span>{totalPureAlcohol} ml</span>
+                                            </div>
+                                            {totalAlcoholInOrganism && (<div className='d-flex justify-content-between'>
+                                                <strong>Promile po wypiciu:</strong>
+                                                <span>{totalAlcoholInOrganism}‰</span>
+                                            </div>)}
+                                            {timeToSober && (
+                                                <div className='d-flex justify-content-between'>
+                                                    <strong>Pokaż się na wykładzie po:</strong>
+                                                    <span>{timeToSober}</span>
+                                                </div>
+                                            )}
+                                        </>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                            <div className="text-center">
+                                {/*<a className="btn btn-outline-dark m-3">btn1</a>*/}
+                            </div>
+                        </div>
                     </div>
-                </>
-            )}
-            </div>
-        </div>
-        </div>
-        <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-            <div className="text-center">
-                <a className="btn btn-outline-dark m-3" href="https://www.google.pl">btn1</a>
-                <a className="btn btn-outline-dark mx-3" href="https://www.google.pl">btn2</a>
-            </div>
-        </div>
-    </div>
-    </div>
+                </div>)
+            }
+        </>
     )
 }
 
--- src/service/AlcoholFunctions.js
+++ src/service/AlcoholFunctions.js
@@ -5,5 +5,20 @@ export const alcoholTotalPure = (alcoholConcentration, alcoholSize, amount) => {
     return amount === 0 ? 0 : alcoholSize * alcoholConcentration * amount;
 }
 export const alcoholInOrganism = (alcoholAmount, weight) => {
-    return alcoholAmount === 0 ? 0 : (alcoholAmount / (weight * 0.7)).toFixed(2)
+    return alcoholAmount === 0 || !weight ? undefined : (alcoholAmount / (weight * 0.7)).toFixed(2)
 }
+/**
+ *
+ * @param alcoholPercent procent stężenia alkoholu we krwi
+ * @returns {string|string} jeśli nie parametru zwracam 0 w przeciwnym wypadku zwracam
+ */
+export const timeToBeSober = alcoholPercent => {
+    if (alcoholPercent === 0 || !alcoholPercent) {
+        return undefined;
+    }
+    else {
+        const hours = Math.floor(alcoholPercent / 0.15);
+        const minutes = Math.round((alcoholPercent / 0.15 - hours) * 60);
+        return `${hours} godz. ${minutes} min.`;
+    }
+};
